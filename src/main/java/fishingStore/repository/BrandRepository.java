4dba94ae2bfd5abf410b4d0feaf7c654c8981049
RaulBogar <100532602+RaulBogar@users.noreply.github.com>
2023-10-02 18:43:55 +0300

    Update BrandRepository.java

--- src/main/java/fishingStore/repository/BrandRepository.java
+++ src/main/java/fishingStore/repository/BrandRepository.java
@@ -3,7 +3,7 @@ package fishingStore.repository;
 import fishingStore.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//add some changes
 
 @Repository
 public interface BrandRepository extends JpaRepository<Brand,Long> {
